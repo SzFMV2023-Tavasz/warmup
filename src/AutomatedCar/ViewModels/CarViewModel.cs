40c134c85096e00df0466c41c811b27f13b9d8b8
Básthy Ádám <basthyadi@gmail.com>
2023-03-10 14:23:09 +0100

    Fix DashboardView
    Dashboard couldn't reach the internal DummyPacket.

--- src/AutomatedCar/ViewModels/CarViewModel.cs
+++ src/AutomatedCar/ViewModels/CarViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace AutomatedCar.ViewModels
 {
     using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -10,9 +11,13 @@
     public class CarViewModel : WorldObjectViewModel
     {
         public AutomatedCar Car { get; set; }
+
+        public IReadOnlyDummyPacket DummyPacket { get; set; }
         public CarViewModel(AutomatedCar car) : base(car)
         {
             this.Car = car;
+
+            this.DummyPacket = car.VirtualFunctionBus.DummyPacket;
         }
     }
 }
