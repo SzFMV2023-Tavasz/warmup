db46eea014cfee34774e19f857d5b6b8dc2965a8
FIJunior <farkas.istvan@stud.uni-obuda.hu>
2023-03-12 16:36:07 +0100

    kész feladat, dummysensor osztály létrehozva

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,30 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DummySensor : SystemComponent
+    {
+        DummyPacket dummyPacket;
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            var mKor = World.Instance.WorldObjects[0];
+            var mAuto = World.Instance.ControlledCar;
+
+            this.dummyPacket.DistanceX = mAuto.X - mKor.X;
+            this.dummyPacket.DistanceY = mAuto.Y - mKor.Y;
+
+        }
+    }
+}
