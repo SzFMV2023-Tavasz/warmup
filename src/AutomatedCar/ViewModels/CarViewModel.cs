7acebc3ccccf1b199ac5987fe708ccaf072c79e2
veladam01 <veladam01@gmail.com>
2023-03-12 19:10:58 +0100

    Implemented and tested DummySensor

--- src/AutomatedCar/ViewModels/CarViewModel.cs
+++ src/AutomatedCar/ViewModels/CarViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace AutomatedCar.ViewModels
 {
     using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -9,10 +10,13 @@
 
     public class CarViewModel : WorldObjectViewModel
     {
+        public VirtualFunctionBus VirtualFunctionBus { get; set; }
+
         public AutomatedCar Car { get; set; }
         public CarViewModel(AutomatedCar car) : base(car)
         {
             this.Car = car;
+            this.VirtualFunctionBus = Car.VirtualFunctionBus;
         }
     }
 }
