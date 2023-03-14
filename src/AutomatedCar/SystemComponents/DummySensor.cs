f4bf9643f6c92cb69aea91a43630044f5de9c333
™kukec™ ™kukec™ <kukimsd@gmail.com>
2023-03-14 13:16:41 +0100

    Dummy Sensor added

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,30 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+
+    class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus): base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            var cirle = World.Instance.WorldObjects[0];
+            var controlled_car = World.Instance.ControlledCar;
+
+            this.dummyPacket.DistanceX = controlled_car.X - cirle.X;
+            this.dummyPacket.DistanceY = controlled_car.Y - cirle.Y;
+        }
+    }
+}
--- src/AutomatedCar/ViewModels/CarViewModel.cs
+++ src/AutomatedCar/ViewModels/CarViewModel.cs
@@ -6,13 +6,16 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using SystemComponents.Packets;
 
     public class CarViewModel : WorldObjectViewModel
     {
         public AutomatedCar Car { get; set; }
+        public IReadOnlyDummyPacket DummyPacket { get; set; }
         public CarViewModel(AutomatedCar car) : base(car)
         {
             this.Car = car;
+            this.DummyPacket = car.VirtualFunctionBus.DummyPacket;
         }
     }
 }
