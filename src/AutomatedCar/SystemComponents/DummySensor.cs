a202996194e025d284d941392c971bf8c14ca518
Solt Tamás <solttamas1998@gmail.com>
2023-03-14 13:26:26 +0100

    Implemented DummySensor.cs

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,34 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DummySensor : SystemComponent
+    {
+        private int circlePosX;
+        private int circlePosY;
+
+        DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus VFB)
+            : base(VFB)
+        {
+            this.dummyPacket = new DummyPacket();
+            VFB.DummyPacket = this.dummyPacket;
+            var circle = World.Instance.WorldObjects.Where(x => nameof(x) == nameof(Circle)).FirstOrDefault();
+        }
+
+        public override void Process()
+        {
+            var currentCar = World.Instance.ControlledCar;
+
+            this.dummyPacket.DistanceX = this.circlePosX - currentCar.X;
+            this.dummyPacket.DistanceY = this.circlePosY - currentCar.Y;
+        }
+    }
+}
