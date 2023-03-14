c8b9eb34444573d8758610e392c8aef97452233f
Solt Tamás <solttamas1998@gmail.com>
2023-03-14 13:46:52 +0100

    Fixed StyleCop warnings in DummySensor.cs

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -4,6 +4,9 @@
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
 
+    /// <summary>
+    /// Implementation of the dummy sensor, which calculate the X and Y distance between the circle WorldObject and the current Car.
+    /// </summary>
     public class DummySensor : SystemComponent
     {
         private int circlePosX;
@@ -11,17 +14,24 @@
 
         private DummyPacket dummyPacket;
 
-        public DummySensor(VirtualFunctionBus VFB)
-            : base(VFB)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DummySensor"/> class.
+        /// </summary>
+        /// <param name="vbf"> The virtual function bus. </param>
+        public DummySensor(VirtualFunctionBus vbf)
+            : base(vbf)
         {
             this.dummyPacket = new DummyPacket();
-            VFB.DummyPacket = this.dummyPacket;
+            vbf.DummyPacket = this.dummyPacket;
             var circle = World.Instance.WorldObjects.Where(x => nameof(x) == nameof(Circle)).FirstOrDefault();
 
             this.circlePosX = circle.X;
             this.circlePosY = circle.Y;
         }
 
+        /// <summary>
+        /// Calculates the distancce between the circle and the car.
+        /// </summary>
         public override void Process()
         {
             var currentCar = World.Instance.ControlledCar;
