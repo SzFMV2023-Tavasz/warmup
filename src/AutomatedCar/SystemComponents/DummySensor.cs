08d1e9813222e56763288e592cff649222ab003e
Martin Pintér <pmrtn@stud.uni-obuda.hu>
2023-03-15 16:56:43 +0100

    Unnecessary usings removed

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -2,11 +2,6 @@
 {
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class DummySensor : SystemComponent
     {
