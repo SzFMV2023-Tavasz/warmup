194d4c83ddb5978a7d991ba982ea96ee60322121
Martin Pintér <pmrtn@stud.uni-obuda.hu>
2023-03-15 13:35:17 +0100

    DummySensor created

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,35 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class DummySensor : SystemComponent
+    {
+        private int objX;
+        private int objY;
+        private DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.objX = World.Instance.WorldObjects[0].X;
+            this.objY = World.Instance.WorldObjects[0].Y;
+
+            this.dummyPacket = new();
+            this.virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            this.objX = World.Instance.WorldObjects[0].X;
+            this.objY = World.Instance.WorldObjects[0].Y;
+
+            this.dummyPacket.DistanceX = this.objX - World.Instance.ControlledCar.X;
+            this.dummyPacket.DistanceY = this.objY - World.Instance.ControlledCar.Y;
+        }
+    }
+}
