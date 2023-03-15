ad887d3d53d2c46dfa456885aaffa0be8813f3b3
Szimilkó Virág <szimiviri11@stud.uni-obuda.hu>
2023-03-15 16:02:50 +0100

    create DummySensor.cs

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,23 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            WorldObject circleObject = World.Instance.WorldObjects[0];
+            AutomatedCar carObject = World.Instance.ControlledCar;
+            this.dummyPacket.DistanceX = carObject.X - circleObject.X;
+            this.dummyPacket.DistanceY = carObject.Y - circleObject.Y;
+        }
+    }
+}
