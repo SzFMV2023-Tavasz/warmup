b821e2334c053d49a30986c96bb77cb4cc61665d
Andras Haut <larkinmaster@gmail.com>
2023-03-16 01:30:05 +0100

    Add documentation to DummySensor.cs

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -8,10 +8,17 @@
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
 
+    /// <summary>
+    /// Dummy sensor implementation.
+    /// </summary>
     public class DummySensor : SystemComponent
     {
         private DummyPacket dummyPacket;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DummySensor"/> class.
+        /// </summary>
+        /// <param name="virtualFunctionBus">The virtual function bus component of our system.</param>
         public DummySensor(VirtualFunctionBus virtualFunctionBus)
             : base(virtualFunctionBus)
         {
@@ -19,6 +26,9 @@
             virtualFunctionBus.DummyPacket = this.dummyPacket;
         }
 
+        /// <summary>
+        /// Method for calculating the distance between the currently controlled car and the dummy object (circle).
+        /// </summary>
         public override void Process()
         {
             var circle = (Circle)World.Instance.WorldObjects.Where(worldObject => worldObject is Circle).First();
