e04f21d870cfc363964de61480e4d031e491a4f2
Andras Haut <larkinmaster@gmail.com>
2023-03-24 15:11:23 +0100

    Enhance the distance calculation massively
    The distance calculation now uses the actual points of the geometries
    instead of the rotationpoint marked by the x,y coordinates or the Bounds.
    The current version also takes the object's rotation into account and
    the calculated x,y distances are absolute and overlapping results in 0.

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -7,6 +7,7 @@
     using System.Threading.Tasks;
     using AutomatedCar.Models;
     using AutomatedCar.SystemComponents.Packets;
+    using Avalonia;
 
     /// <summary>
     /// Dummy sensor implementation.
@@ -34,6 +35,14 @@
             var circle = (Circle)World.Instance.WorldObjects.Where(worldObject => worldObject is Circle).First();
             var car = World.Instance.ControlledCar;
 
+            (int distanceX, int distanceY) = GetDistances2(car, circle);
+
+            this.dummyPacket.DistanceX = distanceX;
+            this.dummyPacket.DistanceY = distanceY;
+        }
+
+        private static (int distanceX, int distanceY) GetDistances1(AutomatedCar car, Circle circle)
+        {
             int carLeft = car.X - (int)car.Geometry.Bounds.Center.X + (int)car.Geometry.Bounds.Left;
             int carRight = car.X - (int)car.Geometry.Bounds.Center.X + (int)car.Geometry.Bounds.Right;
             int circleLeft = circle.X - circle.Radius;
@@ -56,10 +65,89 @@
                 distanceY = (circleBottom < carTop) ? carTop - circleBottom : circleTop - carBottom;
             }
 
-            this.dummyPacket.DistanceX = distanceX;
-            this.dummyPacket.DistanceY = distanceY;
+            return (distanceX, distanceY);
         }
 
-        private static double RotationConverter(double rotation) => -rotation + 90;
+        private static (int distanceX, int distanceY) GetDistances2(AutomatedCar car, Circle circle)
+        {
+            var carPointsRotated = RotatePoints(car);
+            var circlePointsRotated = RotatePoints(circle);
+
+            double carLeft = car.X + carPointsRotated.x.Min();
+            double circleRight = circle.X + circlePointsRotated.x.Max();
+
+            double distanceX = 0;
+            if (carLeft > circleRight)
+            {
+                distanceX = carLeft - circleRight;
+            }
+            else
+            {
+                double carRight = car.X + carPointsRotated.x.Max();
+                double circleLeft = circle.X + circlePointsRotated.x.Min();
+                if (circleLeft > carRight)
+                {
+                    distanceX = circleLeft - carRight;
+                }
+            }
+
+            double carTop = car.Y + carPointsRotated.y.Min();
+            double circleBottom = circle.Y + circlePointsRotated.y.Max();
+
+            double distanceY = 0;
+            if (carTop > circleBottom)
+            {
+                distanceY = carTop - circleBottom;
+            }
+            else
+            {
+                double carBottom = car.Y + carPointsRotated.y.Max();
+                double circleTop = circle.Y + circlePointsRotated.y.Min();
+                if (circleTop > carBottom)
+                {
+                    distanceY = circleTop - carBottom;
+                }
+            }
+
+            return ((int)distanceX, (int)distanceY);
+        }
+
+        /// <summary>
+        /// The function takes the points of the polys of the WorldObject parameter and calculates their distances from the RotationPoint and their respective angles;
+        /// from these and the Rotation property of the WorldObject, the function calculates the rotation corrected points RELATIVE to the RotationPoint
+        /// AND inverts the resulting Y coordinates so it correlates with our coordinate system where the Y axis increases towards the bottom side.
+        /// In practice, if you want to get the true position of a point of an object's geometry,
+        /// you have to add these values of the point to the x,y coordinates (which actually indicate the absolute position of the RotationPoint) of the object.
+        /// </summary>
+        /// <param name="worldObject">The WorldObject to be transformed.</param>
+        /// <returns>Returns respective lists of X and Y coordinates, that indicate their position relative to the RotationPoint of the WorldObject.</returns>
+        private static (List<double> x, List<double> y) RotatePoints(WorldObject worldObject)
+        {
+            (List<double> x, List<double> y) points = new ();
+            points.x = new List<double>();
+            points.y = new List<double>();
+            var rotationPoint = new Avalonia.Point(worldObject.RotationPoint.X, worldObject.RotationPoint.Y);
+
+            foreach (var geometry in worldObject.Geometries)
+            {
+                foreach (var point in geometry.Points)
+                {
+                    double distance = GetEuclidianDistance(point, rotationPoint);
+                    double phi = GetAngle(point, rotationPoint) + Deg2Rad(-worldObject.Rotation);
+                    points.x.Add(Math.Cos(phi) * distance);
+                    points.y.Add(-Math.Sin(phi) * distance);
+                }
+            }
+
+            return points;
+        }
+
+        private static double GetEuclidianDistance(Point point, Point rotationPoint) => Math.Sqrt(Math.Pow(point.X - rotationPoint.X, 2) + Math.Pow(point.Y - rotationPoint.Y, 2));
+
+        private static double ConvertRotation(double rotation) => -rotation + 90;
+
+        private static double Deg2Rad(double degree) => (Math.PI / 180) * degree;
+
+        private static double GetAngle(Point point, Point rotationPoint) => Math.Atan2(rotationPoint.Y - point.Y, point.X - rotationPoint.X);
     }
 }
